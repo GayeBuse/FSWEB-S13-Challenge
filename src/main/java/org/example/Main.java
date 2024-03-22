23777c92991abc018163e585a08a8d6df270b8fa
GayeBuse <gayebuseozkan@gmail.com>
2024-03-22 15:28:25 +0300

    finish solve

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,8 @@ import org.example.enums.Plan;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Healthplan h1 = new Healthplan(1, "GAYE SİGORTA", Plan.BASIC);
-        Healthplan h2 = new Healthplan(2, "BUSE SİGORTA", Plan.OPSIYONEL);
+        Healthplan h1 = new Healthplan(1, "GAYE ", Plan.BASIC);
+        Healthplan h2 = new Healthplan(2, "BUSE ", Plan.OPSIYONEL);
 
         System.out.println(h1);
         System.out.println(h2);
